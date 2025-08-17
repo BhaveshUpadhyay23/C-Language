af8c5be48c275ab4a47d8791badbb07dc73f286e
Bhavesh Upadhyay <bhaveshu20@gmail.com>
2025-08-17 15:48:06 +0530

    initial commit

---
+++ variables.c
@@ -0,0 +1,13 @@
+// VARIABLES
+// Variable is the name of a memory location which stores some data.
+
+#include<stdio.h>
+int main(){
+    int age = 19;
+    char hashtag = '#';
+    float pi = 3.14;
+    printf("Age: %d\n", age);
+    printf("%c\n",hashtag);
+
+    return 0;
+}
